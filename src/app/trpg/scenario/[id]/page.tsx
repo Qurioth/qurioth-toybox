a7fbab132f76b5e0096491358db2042d765ee0f2
Qurioth <dream.night7314@gmail.com>
2024-11-14 01:21:33 +0900

    コピーライト修正

--- src/app/trpg/scenario/[id]/page.tsx
+++ src/app/trpg/scenario/[id]/page.tsx
@@ -6,8 +6,8 @@ import remarkGfm from "remark-gfm";
 const copyright = `
 本作は、「株式会社アークライト」及び「株式会社KADOKAWA」が権利を有する『クトゥルフ神話TRPG』シリーズの二次創作物です。
 
-Call of Cthulhu is copyright ©1981, 2015, 2019 by Chaosium Inc. ;all rights reserved. Arranged by Arclight Inc.
-Call of Cthulhu is a registered trademark of Chaosium Inc.
+Call of Cthulhu is copyright ©1981, 2015, 2019 by Chaosium Inc. ;all rights reserved. Arranged by Arclight Inc.  
+Call of Cthulhu is a registered trademark of Chaosium Inc.  
 PUBLISHED BY KADOKAWA CORPORATION　「クトゥルフ神話TRPG」「新クトゥルフ神話TRPG」
 `;
 
