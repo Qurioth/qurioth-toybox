96e94133315642ec8e2979496310b95e15e6afdf
Qurioth <dream.night7314@gmail.com>
2025-05-06 19:18:00 +0900

    ページタイトル追加/シナリオ修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,10 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Qurioth's Toybox",
+  title: {
+    template: "%s | Qurioth's Toybox",
+    default: "Qurioth's Toybox",
+  },
   description:
     "In [Qurioth's Toybox], Qurioth freely publishes and creates tools and scenarios related to Table Talk RPGs. If you have time to translate this English text, it would be more useful to study for one of the qualifications!",
 };
