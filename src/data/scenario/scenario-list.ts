83bab2974f8bf15a44191f6f065fe956aa88a745
Qurioth <dream.night7314@gmail.com>
2026-05-26 23:13:39 +0900

    シナリオ「濡れた手の泡沫」追加

--- src/data/scenario/scenario-list.ts
+++ src/data/scenario/scenario-list.ts
@@ -1,4 +1,5 @@
 import agnusDeiQuiTollisPeccataMundi from "@/data/scenario/agnus-dei-qui-tollis-peccata-mundi";
+import bubbleOnWetHands from "@/data/scenario/bubble-on-wet-hands";
 import dasDornroschenDesWahnsinnigenKonigs from "@/data/scenario/das-dornröschen-des-wahnsinnigen-königs";
 import theSoundOfSteppingOnFog from "@/data/scenario/the-sound-of-stepping-on-fog";
 import silentJourney from "@/data/scenario/silent-journey";
@@ -14,6 +15,14 @@ const list: {
 　舞台は現代の日本。探索者たちが教会へ訪れたときから事件は展開する。  
 　なお、あらかじめ探索者同士は知り合いだったほうが、ゲームはスムーズに展開する。`,
     markdown: agnusDeiQuiTollisPeccataMundi,
+  },
+  BubbleOnWetHands: {
+    title: "濡れた手の泡沫",
+    overview: `
+　このシナリオは"新クトゥルフ神話 TRPG ルールブック"に対応したシナリオで、探索者 3 ～ 5 人向けにデザインされている。  
+　プレイ時間は探索者の作成時間を含まずに 5 ～ 6 時間程度だろう。  
+　舞台は現代の日本。探索者たちは共通の知人である**沖嶋 深月**の依頼で一人の男性を捜し、海沿いの町へ赴くこととなる。`,
+    markdown: bubbleOnWetHands,
   },
   // PalateOfTheCrawling: {
   //   title: "蠢く口蓋",
@@ -39,7 +48,7 @@ const list: {
 　このシナリオは"新クトゥルフ神話TRPG ルールブック"に対応したシナリオで、探索者 3 ～ 5 人向けにデザインされている。  
 　プレイ時間は探索者の作成時間を含まずに 4 ～ 5 時間程度だろう。  
 　舞台は現代の日本。山間の片田舎で事件は展開する。  
-　シナリオの設定上、少なくとも一人の探索者の知人に **笹本 霧江** という大学生がいることとなる。  
+　シナリオの設定上、少なくとも一人の探索者の知人に**笹本 霧江**という大学生がいることとなる。  
 　なお、あらかじめ探索者同士は知り合いだったほうが、ゲームはスムーズに展開する。`,
     markdown: theSoundOfSteppingOnFog,
   },
