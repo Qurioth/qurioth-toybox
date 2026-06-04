64b9027043e172bf582d16c2217c4d7c4c2b87f5
Qurioth <dream.night7314@gmail.com>
2026-06-04 15:29:21 +0900

    サンプルキャラクター修正
    - ファイル移動
    - 表示キャラクターを修正

--- src/app/trpg/charaeno-chart/sample-character/page.tsx
+++ src/app/trpg/charaeno-chart/sample-character/page.tsx
@@ -4,46 +4,28 @@ import CharacterCard from "@/components/CharacterCard";
 import Template from "@/components/Template";
 import { Investigator } from "@/types/Charaeno7th";
 
-import abbeyPriestly from "@/data/abbey-priestly.json";
-import alisaSolera from "@/data/alisa-solera.json";
-import hamuroAmana from "@/data/hamuro-amana.json";
-import hamuroMichiharu from "@/data/hamuro-michiharu.json";
-import hazamaKureha from "@/data/hazama-kureha.json";
-import ibaRena from "@/data/iba-rena.json";
-import josephineGrimaldi from "@/data/josephine-grimaldi.json";
-import kawasakiSora from "@/data/kawasaki-sora.json";
-import kotoneTsuyu from "@/data/kotone-tsuyu.json";
-import kushitaTaiga from "@/data/kushita-taiga.json";
-import uedaKumiko from "@/data/ueda-kumiko.json";
-import yamatoAkira from "@/data/yamato-akira.json";
+import fugakuRairi from "@/data/sample-character/fugaku-rairi.json";
+import hamuroAmana from "@/data/sample-character/hamuro-amana.json";
+import hamuroMichiharu from "@/data/sample-character/hamuro-michiharu.json";
+import hazamaKureha from "@/data/sample-character/hazama-kureha.json";
+import kawasakiSora from "@/data/sample-character/kawasaki-sora.json";
+import yamatoAkira from "@/data/sample-character/yamato-akira.json";
 
 export default function Home() {
-  const abbeyPriestlyJson: Investigator = abbeyPriestly;
-  const alisaSoleraJson: Investigator = alisaSolera;
+  const fugakuRairiJson: Investigator = fugakuRairi;
   const hamuroAmanaJson: Investigator = hamuroAmana;
   const hamuroMichiharuJson: Investigator = hamuroMichiharu;
   const hazamaKurehaJson: Investigator = hazamaKureha;
-  const ibaRenaJson: Investigator = ibaRena;
-  const josephineGrimaldiJson: Investigator = josephineGrimaldi;
   const kawasakiSoraJson: Investigator = kawasakiSora;
-  const kotoneTsuyuJson: Investigator = kotoneTsuyu;
-  const kushitaTaigaJson: Investigator = kushitaTaiga;
-  const uedaKumikoJson: Investigator = uedaKumiko;
   const yamatoAkiraJson: Investigator = yamatoAkira;
 
   const characterList = [
-    { characterData: abbeyPriestlyJson },
-    { characterData: alisaSoleraJson },
-    { characterData: hamuroAmanaJson },
     { characterData: hamuroMichiharuJson },
+    { characterData: hamuroAmanaJson },
     { characterData: hazamaKurehaJson },
-    { characterData: ibaRenaJson },
-    { characterData: josephineGrimaldiJson },
-    { characterData: kawasakiSoraJson },
-    { characterData: kotoneTsuyuJson },
-    { characterData: kushitaTaigaJson },
-    { characterData: uedaKumikoJson },
     { characterData: yamatoAkiraJson },
+    { characterData: fugakuRairiJson },
+    { characterData: kawasakiSoraJson },
   ];
 
   return (
