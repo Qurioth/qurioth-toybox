c612b838dfe7b8432b03dc7ca516be68d1b2d2d5
Qurioth <dream.night7314@gmail.com>
2026-06-04 23:22:53 +0900

    キャラクターカード修正
    - キャラクター名ですみつき括弧で囲われている範囲を削除するように修正
    - キャラクター名で先頭にスペースがある場合は削除するように修正

--- src/app/trpg/charaeno-chart/page.tsx
+++ src/app/trpg/charaeno-chart/page.tsx
@@ -25,8 +25,11 @@ export default function Home() {
     const charaeno7thApiUrl = `https://charaeno.com/api/v1/7th/${characterId}/summary`;
     const json = await getFetch(charaeno7thApiUrl);
 
-    json.name = json.name.replace(/[\(\{\[\<（][^\)\}\]\>]*[\)\}\]\>）]/g, "");
-    json.name = json.name.replace(/\s+$/, "");
+    json.name = json.name.replace(
+      /[\(\{\[\<（【][^\)\}\]\>）】]*[\)\}\]\>）】]/g,
+      "",
+    );
+    json.name = json.name.replace(/(^\s+|\s+$)/, "");
     setCharacterData(json);
   };
 
