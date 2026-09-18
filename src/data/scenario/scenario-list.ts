7d5e9ba316feb3016373a059b152c277d9b229f3
Qurioth <160010864+Qurioth@users.noreply.github.com>
2026-09-18 22:34:56 +0900

    「硝子檻の虜囚は星海にて夢を見る」シナリオ追加 (#44)
    * シナリオ修正
    
    - 「硝子檻の虜囚は星海にて夢を見る」の修正
    
    * シナリオリスト更新
    
    * シナリオ修正
    
    * シナリオ修正
    
    * docs: `硝子檻の虜囚は星海にて夢を見る` 修正
    
    * docs: `硝子檻の虜囚は星海にて夢を見る` 修正
    
    * docs: `硝子檻の虜囚は星海にて夢を見る` 修正
    
    * docs: `硝子檻の虜囚は星海にて夢を見る` 修正
    
    * docs: `硝子檻の虜囚は星海にて夢を見る` 修正
    
    * docs: `硝子檻の虜囚は星海にて夢を見る` 修正
    
    * docs: `硝子檻の虜囚は星海にて夢を見る` 修正
    
    * docs: `硝子檻の虜囚は星海にて夢を見る` 修正
    
    * docs: `硝子檻の虜囚は星海にて夢を見る` 修正
    
    * docs: `硝子檻の虜囚は星海にて夢を見る` 修正
    
    * docs: `硝子檻の虜囚は星海にて夢を見る` 修正
    
    * docs: `硝子檻の虜囚は星海にて夢を見る` 修正
    
    * docs: `硝子檻の虜囚は星海にて夢を見る` 修正
    
    * docs: `硝子檻の虜囚は星海にて夢を見る` 修正
    
    * docs: `硝子檻の虜囚は星海にて夢を見る` 修正
    
    * docs: README 修正
    
    * docs: `硝子檻の虜囚は星海にて夢を見る` 修正
    
    * docs: `硝子檻の虜囚は星海にて夢を見る` 修正
    
    * docs: `硝子檻の虜囚は星海にて夢を見る` 修正
    
    * docs: `硝子檻の虜囚は星海にて夢を見る` 修正
    
    ---------
    
    Co-authored-by: Qurioth <dream.night7314@gmail.com>

--- src/data/scenario/scenario-list.ts
+++ src/data/scenario/scenario-list.ts
@@ -30,12 +30,24 @@ type Scenario = {
 const list: {
   [key: string]: Scenario;
 } = {
-  //   ThePrisonerInTheGlassCageDreamsInTheSeaOfStars: {
-  //     title: "硝子檻の虜囚は星海にて夢を見る",
-  //     markdown: readScenarioMarkdown(
-  //       "the-prisoner-in-the-glass-cage-dreams-in-the-sea-of-stars.md",
-  //     ),
-  //   },
+  ThePrisonerInTheGlassCageDreamsInTheSeaOfStars: {
+    system: "クトゥルフ神話TRPG 7版",
+    title: "硝子檻の虜囚は星海にて夢を見る",
+    titleKana: "がらすかんのりょしゅうはせいかいにてゆめをみる",
+    players: {
+      min: 4,
+      max: 5,
+    },
+    playTimeHours: {
+      min: 7,
+      max: 7,
+    },
+    summary:
+      "現代日本。バーチャルYouTuber 天戌 ノアの配信を見ていると、彼女が探索者たちの名前を呼ぶ。",
+    markdown: readScenarioMarkdown(
+      "the-prisoner-in-the-glass-cage-dreams-in-the-sea-of-stars.md",
+    ),
+  },
   BubbleOnWetHands: {
     system: "クトゥルフ神話TRPG 7版",
     title: "濡れた手の泡沫",
